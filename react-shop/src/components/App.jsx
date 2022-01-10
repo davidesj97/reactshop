12ef35268b1f85f948198070a17f81b507dc9cb3
David Sánchez <davidesj97@gmail.com>
2022-01-10 11:23:29 -0600

    Configurando webpack para cambios en tiempo real

--- react-shop/src/components/App.jsx
+++ react-shop/src/components/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <h1>¡Hola mundo!</h1>
     </>
   );
 };
--- react-shop/webpack.config.js
+++ react-shop/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  mode: 'development',
   resolve: {
     extensions: ['.js', '.jsx'],
   },
@@ -20,7 +21,7 @@ module.exports = {
         }
       },
       {
-        test: /\.html/,
+        test: /\.html$/,
         use: [
           {
             loader: 'html-loader',
