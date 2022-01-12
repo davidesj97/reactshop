2fbcfc8a40a1a0a31d1b3f464311c364a1ca6fa6
David Sánchez <davidesj97@gmail.com>
2022-01-12 13:48:45 -0600

    Creación del custom hook "useGetProducts"

--- react-shop/src/containers/ProductList.jsx
+++ react-shop/src/containers/ProductList.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { useGetProducts } from '../hooks/useGetProducts';
 import { ProductItem } from '@components/ProductItem';
 import '@styles/ProductList.scss';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(async () => {
-    const response = await axios(API);
-    setProducts(response.data)
-  }, []);
+  const products = useGetProducts(API);
 
 	return (
 		<section className="main-container">
 			<div className="ProductList">
         {products.map(product => (
-          <ProductItem key={product.id}/>
+          <ProductItem product={product} key={product.id}/>
         ))}
 			</div>
 		</section>
